e599e909441a5b6153fe8627c8d0cc04efc83543
ptobey <patmictob@gmail.com>
2021-04-07 15:23:31 -0500

    added second half of the pojo

--- src/src/user.java
+++ src/src/user.java
@@ -20,4 +20,76 @@ public class user {
         this.dob = dob;
         this.password = password;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public paymentMethod getPreferredPayment() {
+        return preferredPayment;
+    }
+
+    public int getDollarsAnHour() {
+        return dollarsAnHour;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPreferredPayment(paymentMethod preferredPayment) {
+        this.preferredPayment = preferredPayment;
+    }
+
+    public void setDollarsAnHour(int dollarsAnHour) {
+        this.dollarsAnHour = dollarsAnHour;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
 }
