deae67c97ce933ad77847ac3c75ef33d671c5d27
ptobey <patmictob@gmail.com>
2021-04-07 15:17:02 -0500

    added half of the pojo

---
+++ src/src/paymentMethod.java
@@ -0,0 +1,3 @@
+public enum paymentMethod {
+    CHECK,CASH,DIRECTDEPOSIT
+}
--- src/src/user.java
+++ src/src/user.java
@@ -1,10 +1,23 @@
 public class user {
     private int id;
     private String name;
+    private String username;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String dob;
     private String password;
+    private paymentMethod preferredPayment = paymentMethod.CHECK;
     private int dollarsAnHour;
     private int hoursWorked;
 
-
-
+    public void user(String name, String username, String email, String address, String phoneNumber, String dob, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.dob = dob;
+        this.password = password;
+    }
 }
