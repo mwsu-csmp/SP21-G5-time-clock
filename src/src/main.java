d504449106b87f9827d8785e129e80a64b484f16
ptobey <patmictob@gmail.com>
2021-04-11 22:04:42 -0500

    made some edits

--- src/src/main.java
+++ src/src/main.java
@@ -5,33 +5,16 @@ import java.io.ObjectOutputStream;
 import java.sql.*;
 
 public class main {
-    public static void main( String args[] ) {
+    public static void main(String args[]) {
         Connection c = null;
 
         try {
             Class.forName("user");
             c = DriverManager.getConnection("jdbc:sqlite:users.db");
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
         System.out.println("Opened database successfully");
     }
-    public byte[] makebyte(user modeldata) {
-        try {
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(baos);
-            oos.writeObject(modeldata);
-            byte[] userAsBytes = baos.toByteArray();
-            ByteArrayInputStream bais = new ByteArrayInputStream(userAsBytes);
-            return userAsBytes;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-    user st = new user("name", "username", "email", "address", "phoneNumber", "dob", "password");
-    byte[] x = makebyte(st);
-    }
+}
