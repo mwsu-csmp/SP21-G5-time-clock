fcf72334fa6b99cff5f7924a23f7fc836a8522ab
ptobey <patmictob@gmail.com>
2021-04-11 21:20:06 -0500

    added sqlite library

--- src/src/main.java
+++ src/src/main.java
@@ -6,7 +6,7 @@ public class main {
 
         try {
             Class.forName("user");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c = DriverManager.getConnection("jdbc:sqlite:users.db");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
