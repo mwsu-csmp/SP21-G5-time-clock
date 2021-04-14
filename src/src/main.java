a492e65080500accff426e4f01cdcf347c758e3d
ptobey <patmictob@gmail.com>
2021-04-14 14:15:34 -0500

    April 14 Starting Commit

--- src/src/main.java
+++ src/src/main.java
@@ -1,7 +1,10 @@
 import java.sql.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class main {
+public class main extends Application {
     public static void main(String args[]) {
+        launch(args);
         Connection c = null;
 
         try {
@@ -12,5 +15,12 @@ public class main {
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Login");
+        primaryStage.show();
     }
 }
